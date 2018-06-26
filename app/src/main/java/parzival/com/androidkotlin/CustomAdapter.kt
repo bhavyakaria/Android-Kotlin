8047e9f8439410ed2d9f63732ba02d5d9d872f55
bhavyakaria <bhavyakaria@yahoo.in>
2018-06-26 19:16:01 +0530

    Added click listener function in CustomAdapter file.

--- app/src/main/java/parzival/com/androidkotlin/CustomAdapter.kt
+++ app/src/main/java/parzival/com/androidkotlin/CustomAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import kotlinx.android.synthetic.main.list.view.*
 import java.util.ArrayList
 
@@ -13,6 +14,7 @@ class CustomAdapter(val userList: ArrayList<DataClass>): RecyclerView.Adapter<Cu
         val list:DataClass = userList[position]
         holder.countryName.text = list.countryName
         holder.capital.text = list.capital
+        holder.bind(list)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,6 +29,16 @@ class CustomAdapter(val userList: ArrayList<DataClass>): RecyclerView.Adapter<Cu
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val countryName = itemView.countryName
         val capital = itemView.capital
+
+        fun bind(listViewItem: DataClass) {
+            itemView.setOnClickListener(View.OnClickListener {
+
+                Toast.makeText(
+                        itemView.context,
+                        "Country: " + listViewItem.countryName + "/n Capital: " + listViewItem.capital,
+                        Toast.LENGTH_LONG).show()
+            })
+        }
     }
 
 }
